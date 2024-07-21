cdc3005a4fa8a5c78a11d87f53fe7223c9abeaa6
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-07-21 14:11:10 +0700

    feat: add config & add mongodb connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	routes "noto/internal"
 
+	"noto/internal/config"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	config.ConnectMongoDB()
+
 	app := fiber.New()
 
 	routes.SetupRoutes(app)
