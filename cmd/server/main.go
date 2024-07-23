71efdfeea9dc1d678a845a636a1079052b2442bd
Shiyinq <shiyinq.github.io@gmail.com>
2024-07-23 21:27:26 +0700

    feat: add request logger for HTTP request tracking

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,16 +2,26 @@ package main
 
 import (
 	routes "noto/internal"
+	"os"
 
 	"noto/internal/config"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	config.ConnectMongoDB()
 
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		EnablePrintRoutes: false,
+	})
+
+	app.Use(logger.New(logger.Config{
+		Format:     "[${time}] ${status} - ${method} ${path}\n",
+		TimeFormat: "2006-01-02 15:04:05",
+		Output:     os.Stdout,
+	}))
 
 	routes.SetupRoutes(app)
 
