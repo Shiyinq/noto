74c428a4555dd88bc579c546e9b0ac5b3824a595
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-07-23 08:28:30 +0700

    feat: add IsArchived

--- internal/services/books/model/model.go
+++ internal/services/books/model/model.go
@@ -5,14 +5,16 @@ import (
 )
 
 type Book struct {
-	Title     string    `json:"title" bson:"title"`
-	CreatedAt time.Time `json:"created_at" bson:"createdAt"`
-	UpdatedAt time.Time `json:"updated_at" bson:"updatedAt"`
+	Title      string    `json:"title" bson:"title"`
+	CreatedAt  time.Time `json:"created_at" bson:"createdAt"`
+	UpdatedAt  time.Time `json:"updated_at" bson:"updatedAt"`
+	IsArchived bool      `json:"is_archived" bson:"isArchived"`
 }
 
 type BookResponse struct {
-	ID        string    `json:"id" bson:"_id"`
-	Title     string    `json:"title" bson:"title"`
-	CreatedAt time.Time `json:"created_at" bson:"createdAt"`
-	UpdatedAt time.Time `json:"updated_at" bson:"updatedAt"`
+	ID         string    `json:"id" bson:"_id"`
+	Title      string    `json:"title" bson:"title"`
+	CreatedAt  time.Time `json:"created_at" bson:"createdAt"`
+	UpdatedAt  time.Time `json:"updated_at" bson:"updatedAt"`
+	IsArchived bool      `json:"is_archived" bson:"isArchived"`
 }
