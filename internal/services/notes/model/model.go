4c30b950bf8d64755e5211a03c184ef0a9202133
Shiyinq <shiyinq.github.io@gmail.com>
2024-07-30 19:28:09 +0700

    feat: update notes API integrations with database

--- internal/services/notes/model/model.go
+++ internal/services/notes/model/model.go
@@ -1,7 +1,26 @@
 package model
 
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type Note struct {
-	ID    string `json:"id"`
-	Title string `json:"title"`
-	Text  string `json:"text"`
+	BookId    primitive.ObjectID `json:"book_id" bson:"bookId"`
+	Text      string             `json:"text" bson:"text"`
+	CreatedAt time.Time          `json:"created_at" bson:"createdAt"`
+	UpdatedAt time.Time          `json:"updated_at" bson:"updatedAt"`
+}
+
+type NoteUpdate struct {
+	Text string `json:"text" bson:"text"`
+}
+
+type NoteResponse struct {
+	ID        string             `json:"id" bson:"_id"`
+	BookId    primitive.ObjectID `json:"book_id" bson:"bookId"`
+	Text      string             `json:"text" bson:"text"`
+	CreatedAt time.Time          `json:"created_at" bson:"createdAt"`
+	UpdatedAt time.Time          `json:"updated_at" bson:"updatedAt"`
 }
--- internal/services/notes/repository/repository.go
+++ internal/services/notes/repository/repository.go
@@ -1,60 +1,129 @@
 package repository
 
 import (
+	"context"
 	"errors"
+	"noto/internal/config"
 	model "noto/internal/services/notes/model"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type NoteRepository interface {
-	GetAllNotes() ([]model.Note, error)
-	GetNoteByID(id string) (*model.Note, error)
+	GetAllNotes(bookId string) ([]model.NoteResponse, error)
 	CreateNote(Note *model.Note) (*model.Note, error)
-	UpdateNoteByID(id string, NewNote *model.Note) (*model.Note, error)
-	DeleteNoteByID(id string) error
+	UpdateNote(bookId string, noteId string, note *model.NoteUpdate) (*model.NoteResponse, error)
+	DeleteNote(bookId string, noteId string) error
 }
 
 type NoteRepositoryImpl struct {
-	notes []model.Note
+	notes *mongo.Collection
 }
 
 func NewNoteRepository() NoteRepository {
-	return &NoteRepositoryImpl{notes: []model.Note{}}
+	return &NoteRepositoryImpl{notes: config.DB.Collection("notes")}
 }
 
-func (r *NoteRepositoryImpl) GetAllNotes() ([]model.Note, error) {
-	return r.notes, nil
-}
-
-func (r *NoteRepositoryImpl) GetNoteByID(id string) (*model.Note, error) {
-	for _, note := range r.notes {
-		if note.ID == id {
-			return &note, nil
-		}
+func (r *NoteRepositoryImpl) GetAllNotes(bookId string) ([]model.NoteResponse, error) {
+	var notes []model.NoteResponse
+	objectID, err := primitive.ObjectIDFromHex(bookId)
+	if err != nil {
+		return nil, errors.New("invalid bookId format")
 	}
-	return nil, errors.New("note not found")
+
+	filter := bson.M{"bookId": objectID}
+	cursor, err := r.notes.Find(context.Background(), filter)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := cursor.All(context.Background(), &notes); err != nil {
+		return nil, err
+	}
+
+	return notes, nil
 }
 
 func (r *NoteRepositoryImpl) CreateNote(note *model.Note) (*model.Note, error) {
-	var _ = append(r.notes, *note)
+	var idErr error
+	note.CreatedAt = time.Now()
+	note.UpdatedAt = time.Now()
+	note.BookId, idErr = primitive.ObjectIDFromHex(note.BookId.Hex())
+	if idErr != nil {
+		return nil, errors.New("invalid ID format")
+	}
+
+	_, err := r.notes.InsertOne(context.Background(), note)
+	if err != nil {
+		return nil, err
+	}
+
 	return note, nil
 }
 
-func (r *NoteRepositoryImpl) UpdateNoteByID(id string, newNote *model.Note) (*model.Note, error) {
-	for i, note := range r.notes {
-		if note.ID == id {
-			r.notes[i] = *newNote
-			return newNote, nil
-		}
+func (r *NoteRepositoryImpl) UpdateNote(bookId string, noteId string, note *model.NoteUpdate) (*model.NoteResponse, error) {
+	bookObjectID, err := primitive.ObjectIDFromHex(bookId)
+	if err != nil {
+		return nil, errors.New("invalid bookId format")
 	}
-	return nil, errors.New("note not found")
+
+	noteObjectID, err := primitive.ObjectIDFromHex(noteId)
+	if err != nil {
+		return nil, errors.New("invalid noteId format")
+	}
+
+	filter := bson.M{"_id": noteObjectID, "bookId": bookObjectID}
+	update := bson.M{
+		"$set": bson.M{
+			"text":      note.Text,
+			"updatedAt": time.Now(),
+		},
+	}
+
+	result := r.notes.FindOneAndUpdate(context.Background(), filter, update, options.FindOneAndUpdate().SetReturnDocument(options.After))
+
+	if result.Err() != nil {
+		if result.Err() == mongo.ErrNoDocuments {
+			return nil, errors.New("note not found")
+		}
+		return nil, result.Err()
+	}
+
+	var updatedNote model.NoteResponse
+	err = result.Decode(&updatedNote)
+	if err != nil {
+		return nil, err
+	}
+
+	return &updatedNote, nil
 }
 
-func (r *NoteRepositoryImpl) DeleteNoteByID(id string) error {
-	for i, note := range r.notes {
-		if note.ID == id {
-			var _ = append(r.notes[:i], r.notes[i+1:]...)
-			return nil
-		}
+func (r *NoteRepositoryImpl) DeleteNote(bookId string, noteId string) error {
+	bookObjectID, err := primitive.ObjectIDFromHex(bookId)
+	if err != nil {
+		return errors.New("invalid bookId format")
 	}
-	return errors.New("note not found")
+
+	noteObjectID, err := primitive.ObjectIDFromHex(noteId)
+	if err != nil {
+		return errors.New("invalid noteId format")
+	}
+
+	filter := bson.M{"_id": noteObjectID, "bookId": bookObjectID}
+
+	deleted, err := r.notes.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		return err
+	}
+
+	if deleted.DeletedCount == 0 {
+		return errors.New("note not found or not deleted")
+	}
+
+	return nil
 }
