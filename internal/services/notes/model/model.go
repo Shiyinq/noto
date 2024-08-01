dabf90db708d1f1f079aabb580e3c3092872bc37
Shiyinq <shiyinq.github.io@gmail.com>
2024-08-01 19:10:58 +0700

    refactor: enhance create note

--- internal/services/notes/model/model.go
+++ internal/services/notes/model/model.go
@@ -6,7 +6,12 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type Note struct {
+type NoteCreateSwagger struct {
+	Text string `json:"text" bson:"text"`
+}
+
+type NoteCreate struct {
+	ID        primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 	BookId    primitive.ObjectID `json:"book_id" bson:"bookId"`
 	Text      string             `json:"text" bson:"text"`
 	CreatedAt time.Time          `json:"created_at" bson:"createdAt"`
