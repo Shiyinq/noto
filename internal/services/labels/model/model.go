f0d382c7042c706649f133cec51bcce9c1bbceaf
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-08-01 18:43:28 +0700

    refactor: improve body and response for create label endpoint

--- internal/services/labels/model/model.go
+++ internal/services/labels/model/model.go
@@ -2,12 +2,19 @@ package model
 
 import (
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type Label struct {
-	Name      string    `json:"name" bson:"name"`
-	CreatedAt time.Time `json:"created_at" bson:"createdAt"`
-	UpdatedAt time.Time `json:"updated_at" bson:"updatedAt"`
+type LabelCreateSwagger struct {
+	Name string `json:"name" bson:"name"`
+}
+
+type LabelCreate struct {
+	ID        primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Name      string             `json:"name" bson:"name"`
+	CreatedAt time.Time          `json:"created_at" bson:"createdAt"`
+	UpdatedAt time.Time          `json:"updated_at" bson:"updatedAt"`
 }
 
 type LabelUpdate struct {
