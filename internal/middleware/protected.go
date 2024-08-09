8f914ed797def889fd6349a3790237353649e59f
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-08-09 18:45:51 +0700

    feat: add protected middleware

---
+++ internal/middleware/protected.go
@@ -0,0 +1,47 @@
+package middleware
+
+import (
+	"net/http"
+	"noto/internal/config"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func Protected(c *fiber.Ctx) error {
+	authHeader := c.Get("Authorization")
+	if authHeader == "" {
+		return c.Status(http.StatusUnauthorized).JSON(fiber.Map{"message": "Missing authorization header"})
+	}
+
+	parts := strings.Split(authHeader, " ")
+	if len(parts) != 2 || parts[0] != "Bearer" {
+		return c.Status(http.StatusUnauthorized).JSON(fiber.Map{"message": "Invalid authorization header"})
+	}
+
+	tokenString := parts[1]
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fiber.NewError(http.StatusUnauthorized, "Invalid signing method")
+		}
+		return config.JWTSecret, nil
+	})
+
+	if err != nil || !token.Valid {
+		return c.Status(http.StatusUnauthorized).JSON(fiber.Map{"message": "Invalid or expired token"})
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return c.Status(http.StatusUnauthorized).JSON(fiber.Map{"message": "Invalid token claims"})
+	}
+
+	c.Locals("userID", claims["id"])
+	c.Locals("userEmail", claims["email"])
+	c.Locals("userName", claims["name"])
+	c.Locals("jwtExp", claims["exp"])
+
+	return c.Next()
+}
