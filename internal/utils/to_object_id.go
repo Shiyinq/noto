dfdb5867a38840cb5c00a6870d51b49e964f6ec8
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-08-10 15:30:49 +0700

    feat: change error type

--- internal/utils/to_object_id.go
+++ internal/utils/to_object_id.go
@@ -1,14 +1,15 @@
 package utils
 
 import (
-	"github.com/gofiber/fiber/v2"
+	"errors"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func ToObjectID(id string) (primitive.ObjectID, error) {
 	objectID, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
-		return primitive.NilObjectID, fiber.NewError(fiber.StatusBadRequest, "Invalid User ID format")
+		return primitive.NilObjectID, errors.New("invalid user id format")
 	}
 
 	return objectID, nil
--- internal/utils/user_id.go
+++ internal/utils/user_id.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"errors"
+
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -8,12 +10,12 @@ import (
 func GetUserID(c *fiber.Ctx) (primitive.ObjectID, error) {
 	userId, ok := c.Locals("userID").(string)
 	if !ok {
-		return primitive.NilObjectID, fiber.NewError(fiber.StatusUnauthorized, "User ID not found")
+		return primitive.NilObjectID, errors.New("user id not found")
 	}
 
 	objUserId, err := primitive.ObjectIDFromHex(userId)
 	if err != nil {
-		return primitive.NilObjectID, fiber.NewError(fiber.StatusBadRequest, "Invalid User ID format")
+		return primitive.NilObjectID, errors.New("invalid user id format")
 	}
 
 	return objUserId, nil
